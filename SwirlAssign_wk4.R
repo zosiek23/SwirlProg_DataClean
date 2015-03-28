2dc7c0c2d4734d04fb917c6f22ac42acc8c5a54f
zosiek23 <zosiek23@hotmail.com>
2015-03-28 20:51:53 +0000

    Swirl programming _ wk 4 --> all completed

---
+++ SwirlAssign_wk4.R
@@ -0,0 +1,5 @@
+install.packages("swirl")
+library(swirl)
+install_from_swirl("Getting and Cleaning Data")
+swirl()
+
---
+++ script7.R
@@ -0,0 +1,8 @@
+# select() the id, class, midterm, and final columns
+# (in that order) and store the result in gradebook.
+#
+gradebook <- students4 %>%
+  select(id, class, midterm, final)%>%
+  
+  ### Your code here %>%
+  print
